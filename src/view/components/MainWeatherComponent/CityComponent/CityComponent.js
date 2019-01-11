ac49dd28f5a434a18f544a8664d7ee0d238792c8
Vojkan <t.vojkan@gmail.com>
2019-01-11 17:58:23 +0100

    date opacity

--- src/view/components/MainWeatherComponent/CityComponent/CityComponent.js
+++ src/view/components/MainWeatherComponent/CityComponent/CityComponent.js
@@ -1,32 +1,17 @@
 import React from "react";
-import $ from 'jquery';
 import './cityComponent.css';
-
-
-const click = () =>{
-
-    $('.dropdown-el').click(function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        $(this).toggleClass('expanded');
-        $('#'+$(event.target).attr('for')).prop('checked',true);
-      });
-      $(document).click(function() {
-        $('.dropdown-el').removeClass('expanded');
-    });
-}
+import {click} from './clickFunction.js';
 
 
 const CityComponent = () =>{
-    
-
     return (
-        <div>
+        <React.Fragment>
             <span onClick={click} class="dropdown-el">
                 <input type="radio" name="city" defaultChecked id="beograd"/><label htmlFor="beograd">BEOGRAD</label>
                 <input type="radio" name="city" id="novisad"/><label htmlFor="novisad">NOVI SAD</label> 
-             </span>
-        </div>  
+            </span>
+            <p className="weather-date">9 AUGUST</p>
+        </React.Fragment>
     )
 }
 
