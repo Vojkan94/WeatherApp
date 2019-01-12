28fa5fedc9338b7d393ad04404c167c37bc58639
Vojkan <t.vojkan@gmail.com>
2019-01-12 19:13:25 +0100

    current date

--- src/view/components/CurrentWeatherDetails/CurrentWeatherDetails.js
+++ src/view/components/CurrentWeatherDetails/CurrentWeatherDetails.js
@@ -8,7 +8,7 @@ const CurrentWeatherDetails = ({city})=>{
         <div><p className="cwd-details">{humidity * 100}%</p><p className="cwd-details-text">Humidity</p></div>
         <div><p className="cwd-details">{dewPoint}<span>&deg;</span></p><p className="cwd-details-text">Dev Pt.</p></div>
         <div><p className="cwd-details">{uvIndex}/10</p><p className="cwd-details-text">UV Index</p></div>
-        <div><p className="cwd-details">{visibility} km</p><p className="cwd-details-text">Visibility</p></div>
+        <div><p className="cwd-details">{visibility.toFixed(1)} km</p><p className="cwd-details-text">Visibility</p></div>
         </div>
     )
 }
--- src/view/components/MainWeatherComponent/CityComponent/CityComponent.js
+++ src/view/components/MainWeatherComponent/CityComponent/CityComponent.js
@@ -4,13 +4,17 @@ import {click} from './clickFunction.js';
 
 
 const CityComponent = () =>{
+    const date = new Date();
+    const month = date.toLocaleString('en-us', { month: 'long' }).toUpperCase();
+    let day = `${date.getDate()}`;
+    day = day.length === 1 ? `0${day}` : day;
     return (
         <Fragment>
             <span onClick={click} class="dropdown-el">
                 <input type="radio" name="city" defaultChecked id="beograd"/><label htmlFor="beograd">BEOGRAD</label>
                 <input type="radio" name="city" id="novisad"/><label htmlFor="novisad">NOVI SAD</label> 
             </span>
-            <p className="weather-date">9 AUGUST</p>
+            <p className="weather-date">{day} {month}</p>
         </Fragment>
     )
 }
