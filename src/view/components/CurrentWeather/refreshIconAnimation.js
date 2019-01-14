6dc90fea7a7b765a59785d96a0c86bc33feb7668
Vojkan <t.vojkan@gmail.com>
2019-01-14 23:50:33 +0100

    icon animation refactoring

--- src/view/components/CurrentWeather/refreshIconAnimation.js
+++ src/view/components/CurrentWeather/refreshIconAnimation.js
@@ -1,14 +1,11 @@
 import $ from 'jquery';
 
 export const refreshIconAnimation = () => {
-    
-
-
 
     let rotate = $('.icon-refresh').attr('style');
     if(rotate != undefined){
 
-        rotate =rotate.split('(');
+        rotate = rotate.split('(');
         rotate = rotate[rotate.length-1].split(")");
         rotate = rotate[0].split('deg');
         rotate = rotate[0]*1 + 360;
@@ -16,13 +13,12 @@ export const refreshIconAnimation = () => {
         $('.icon-refresh').css({
             'transform': `rotate(${rotate}deg)`
         })
+    }else{
+        $('.icon-refresh').css({
+            'transform': 'rotate(360deg)'
+        })
     }
 
-    if($('.icon-refresh').css('transform') === "none"){
-        console.log("nema")
-            $('.icon-refresh').css({
-                'transform': 'rotate(360deg)'
-            })
-    }
+    
  
 }
